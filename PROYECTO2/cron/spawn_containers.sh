17651f96026d839c846a649458b97e8886043463
DavidVela <3626064120101@ingenieria.usac.edu.gt>
2026-03-08 12:58:51 -0600

    generador de carga con contenendoreas low, high CPU y high RAM

--- PROYECTO2/cron/spawn_containers.sh
+++ PROYECTO2/cron/spawn_containers.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-echo "Iniciando contenedores de prueba..."
+echo "Limpiando contenedores anteriores de prueba..."
+docker rm -f low_1 low_2 low_3 highcpu_1 highcpu_2 highram_1 2>/dev/null
 
-# Contenedor de bajo consumo
-docker run -d --rm --name low_test alpine sh -c "sleep 300"
+echo "Creando contenedores de bajo consumo..."
+docker run -d --rm --name low_1 alpine sh -c "sleep 300"
+docker run -d --rm --name low_2 alpine sh -c "sleep 300"
+docker run -d --rm --name low_3 alpine sh -c "sleep 300"
 
-# Contenedor de alto CPU
-docker run -d --rm --name highcpu_test alpine sh -c "while true; do :; done"
+echo "Creando contenedores de alto consumo CPU..."
+docker run -d --rm --name highcpu_1 alpine sh -c "while true; do :; done"
+docker run -d --rm --name highcpu_2 alpine sh -c "while true; do :; done"
 
-echo "Contenedores creados:"
+echo "Creando contenedor de alto consumo RAM..."
+docker run -d --rm --name highram_1 python:3.12-alpine sh -c "python3 -c 'x = bytearray(300 * 1024 * 1024); import time; time.sleep(300)'"
+
+echo
+echo "Contenedores activos:"
 docker ps --format "table {{.Names}}\t{{.Image}}\t{{.Status}}"
